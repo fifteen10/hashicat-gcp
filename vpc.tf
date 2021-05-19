af61d7b1ec1a91c20dfc875ba4d9d9888d55b90c
Anibal <fifteen10.it@gmail.com>
2021-05-19 16:56:54 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,12 @@
-subnets = [
+module "network" {
+  source  = "app.terraform.io/anibal-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+}
